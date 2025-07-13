7f4829d9e25214f1a8759940488f32e59b36110a
kalyani <reachkalyanihere@hotmail.com>
2025-07-13 17:53:44 -0500

    Added Log4j utility

--- src/test/java/com/automationexercise/tests/LoginPageTest.java
+++ src/test/java/com/automationexercise/tests/LoginPageTest.java
@@ -2,6 +2,8 @@ package com.automationexercise.tests;
 
 import com.automationexercise.pages.LoginPage;
 import com.automationexercise.utils.ConfigReader;
+import com.automationexercise.utils.LoggerUtil;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -10,16 +12,20 @@ import org.testng.annotations.Test;
 public class LoginPageTest extends BaseTest{
 
     LoginPage loginPage;
+    Logger log = LoggerUtil.getLogger(LoginPageTest.class);
     String email = ConfigReader.getProperty("emailAddress");
     String pwd = ConfigReader.getProperty("password");
     @BeforeMethod(alwaysRun = true)
     public void loginPageSetup(){
         loginPage = new LoginPage(driver);
+        log.info("Web driver initiated for Login Page Tests..");
     }
 
     @Test(groups = {"Sanity"},priority = 1)
     public void verifyInvalidLoginFailure(){
-        System.out.println("verifyInvalidLoginFailure");
+        log.info("Starting test - verifyInvalidLoginFailure");
+        log.info("Clicking on Login Menu button");
+        log.info("Entering email address");
         loginPage.clickOnLoginMenu();
         loginPage.enterEmailAddress(email);
         loginPage.enterPassword(pwd);
